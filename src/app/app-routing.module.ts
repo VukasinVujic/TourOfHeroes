95ccffdb36aa26807ef3489f02761fcc44065f63
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-22 19:16:46 +0200

    Routing 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
-
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 // import { CommonModule } from "@angular/common";
 
 const routes: Routes = [
   {
-    path: "heroes",
-    component: HeroesComponent
-  }
+    path: '', 
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+  path: "heroes",
+  component: HeroesComponent
+  },
 ];
 
 @NgModule({
