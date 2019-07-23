efe78f1b0cd6eb0e6b75b2868478f9cfa27a5567
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-23 16:22:29 +0200

    Change "of" with "http.get"

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,20 +3,29 @@ import { Observable, of } from "rxjs";
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add("HeroService: fetch heroes");
-    return of(HEROES);
+    // this.messageService.add("HeroService: fetch heroes");
+    // return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message_after_fetching the hero
     this.messageService.add(`HeroService: fetch hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesUrl = "api/heroes";
 }
