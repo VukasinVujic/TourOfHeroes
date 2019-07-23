67b1cd6cafb44df8f1614a6e40e14647c1dc6886
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-23 20:14:40 +0200

    Tap,thing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,15 +20,26 @@ export class HeroService {
     // return of(HEROES);
     return this.http
       .get<Hero[]>(this.heroesUrl)
-      .pipe(catchError(this.handleError<Hero[]>("getHeroes", [])));
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>("getHeroes", [])));
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message_after_fetching the hero
     this.messageService.add(`HeroService: fetch hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
   private heroesUrl = "api/heroes";
+  private handleError<T> (operation = 'operation', result?: T){
+  return (error: any): Observable<T> => {
+    console.error(error);
+    this.log(`${operation} failed: ${error.message}`);
+    return of(result as T);
+  };  
+  }
 }
